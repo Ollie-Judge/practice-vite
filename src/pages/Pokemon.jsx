747b302278e0558d5f5c63cacac1a26c9c2debb2
Ollie-Judge <ollie.judge@hotmail.com>
2024-01-19 17:27:11 +0000

    recieved and output and api response

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -1,12 +1,29 @@
+import { useState, useEffect } from "react";
 import "../style/App.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 function Pokemon() {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setPokemon(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
   return (
     <>
       <Header />
-      <h2>Pokemon</h2>
+      <h2>Pokemon name: {pokemon.name}</h2>
+      <h3>Pokemon ID: {pokemon.id}</h3>
+
       <Footer />
     </>
   );
