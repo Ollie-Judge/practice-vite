0f3c69e5812da6993ce4751d4362ff3fe0a87a69
Ollie-Judge <ollie.judge@hotmail.com>
2024-01-22 15:47:49 +0000

    made the page load a pokemon name andid number with search input

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -1,13 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "../style/App.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 function Pokemon() {
+  const pokemonNameInput = document.getElementById("pokemonNameInput");
   const [pokemon, setPokemon] = useState([]);
 
-  useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+  const searchPokemon = (e) => {
+    e.preventDefault();
+    console.log(pokemonNameInput.value);
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNameInput.value}`)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
@@ -16,14 +19,26 @@ function Pokemon() {
       .catch((err) => {
         console.log(err.message);
       });
-  }, []);
+  };
 
   return (
     <>
       <Header />
       <h2>Pokemon name: {pokemon.name}</h2>
       <h3>Pokemon ID: {pokemon.id}</h3>
-
+      <form id="pokemonForm" action="/pokemon">
+        <input
+          type="text"
+          id="pokemonNameInput"
+          placeholder="Name of Pokemon"
+        />
+        <input
+          type="submit"
+          value="Submit"
+          id="submitPokemonNameButton"
+          onClick={searchPokemon}
+        />
+      </form>
       <Footer />
     </>
   );
